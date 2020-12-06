e9cb3bdbffe41b359c32989c830eeb4e1e847379
Adam Múdry <mudry.ado@gmail.com>
2020-12-06 07:16:19 +0100

    visual change

--- 06/day6.nim
+++ 06/day6.nim
@@ -1,18 +1,22 @@
 import strutils, sequtils, sets
 
-const input : seq[string] = slurp("input.txt").split("\n\n") # Had to delete the last newline in input file...
-var count = 0
+const input = slurp("input.txt").split("\n\n") # Had to delete the last newline in input file...
+var 
+  count = 0
+  l : string
 
-for line in input:
-  var l = line.split().foldl(a&b)
-  var h = toHashSet(l)
-  count += h.len
-echo "Part one: ", count
-count = 0
-
-var hset : HashSet[char]
 for entry in input:
-  var entry_split = entry.split()
+  l = entry.split().foldl(a&b)
+  count += toHashSet(l).len
+echo "Part one: ", count
+
+count = 0
+var
+  entry_split : seq[string]
+  hset : HashSet[char]
+
+for entry in input:
+  entry_split = entry.split()
   for i, line in entry_split:
     if i == 0:
       hset = toHashSet(line)
